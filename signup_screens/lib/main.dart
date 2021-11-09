96f83bb5c4b55ba1618f7ae36a6aaed0fdd4fc63
vpmaharani09 <vpmaharani09@gmail.com>
2021-11-09 23:58:50 +0700

    ui screens

---
+++ signup_screens/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:signup_screens/pages/sign_up.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SignUp(),
+    );
+  }
+}
